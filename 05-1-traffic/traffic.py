e1bb9a4b0ba531c15b2c0e72a94eca0340f35755
elifsyigit <elsenayigit@gmail.com>
2025-04-26 13:59:37 +0300

    refine comments

--- 05-1-traffic/traffic.py
+++ 05-1-traffic/traffic.py
@@ -15,34 +15,26 @@ TEST_SIZE = 0.4
 
 def main():
 
-    # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
 
-    # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
 
-    # Split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
 
-    # Get a compiled neural network
     model = get_model()
 
-    # Fit model on training data
     model.fit(x_train, y_train, epochs=EPOCHS)
 
-    # Evaluate neural network performance
     model.evaluate(x_test,  y_test, verbose=2)
 
-    # Save model to file
     if len(sys.argv) == 3:
         filename = sys.argv[2]
         model.save(filename)
         print(f"Model saved to {filename}.")
-
 
 def load_data(data_dir):
     """
@@ -58,9 +50,6 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-
-    # until nothing left,go through the i th subfolder and label it as i 
-    # i++ and repeat the same process
     images = []
     labels = []
 
